fbf95ba096e6b0da631f2e6ff0cc60a424411125
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-26 12:38:40 +0300

    fifth add new elements

--- js/main.js
+++ js/main.js
@@ -16,11 +16,22 @@
 const enter = input.addEventListener ("keypress", function (e) {
     if (e.keyCode === 13) {
         const text = document.getElementById('input').value.trim();
-        var d1 = document.getElementById('input'); 
-        d1.insertAdjacentHTML('afterend', '<div id="inser2"></div>');
+        document.getElementById("input").value = "";
+        let d1 = document.getElementById('input'); 
+        d1.insertAdjacentHTML('afterend', '<div id="inser2" class="inser2"></div>');
+        d1.insertAdjacentHTML('afterend', '<div id="wrapper" class="wrapper"></div>');
+        let d2 = document.getElementById('wrapper');
+        d2.insertAdjacentHTML('afterBegin', '<spin class="spin"><i class="fa fa-times" onclick="del()"></i></spin>');
+
         const out = document.getElementById('inser2').innerHTML = text;
+        document.getElementById('inser2').classList.add("new-todo", "new");
     }
 });
+
+const del = () => {
+    inser2.remove();
+};
+
 
 
 

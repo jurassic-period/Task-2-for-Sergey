fbe95bc5e68bc70fd86353ef27d55c2b96264e10
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-30 16:11:51 +0300

    seventh add all new features

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
 // By pressing the enter, the value of the entered data is determined in the input,
 //  new Li are created to which this value is transmitted ________________________
 
-const enter = input.addEventListener ("keypress", function (e) {
-    if (e.keyCode === 13) {
-        const text = document.getElementById('input').value.trim();
-        document.getElementById('input').value = '';
-        
-        let newLi = document.getElementById('tameplate');
-        let liClone = newLi.cloneNode(true);
-        newLi.parentNode.appendChild(liClone);
-        newLi.classList.add("block");
-        let textP = document.getElementById('inputText');
-        textP.innerHTML = text;  
-    }
-});
+try {
+    //.content - потому что всё содержимое шаблона завёрнуто в невидимый `document-fragment`
+    let template = document.querySelector('#template').content; 
+    let li = template.querySelector('.tameplate');
+    let ul = document.querySelector('.unordered-list');
+
+    const enter = input.addEventListener ("keypress", function (e) {
+        if (e.keyCode === 13) {
+            const text = document.getElementById('input').value.trim();
+            document.getElementById('input').value = '';
+            
+            let newLi = li.cloneNode(true);
+            ul.appendChild(newLi);
+            // newLi.classList.add("block"); // add class, now don't need
+            let textP = newLi.querySelector('#inputText');
+            textP.innerHTML = text;  
+        }
+    });
+} catch {
+    alert('Please refresh the page');
+}
 
 
 // By clicking, the current 'li' will be deleted.__________________________________
 
-const del = () => {
-    let newLi = document.getElementById('tameplate');
-    newLi.remove();
+const del = (f) => {
+    f.remove();
 };
 
 

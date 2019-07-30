b1e5f752d87dee3ad66a180c777360a91a7a1154
Yuriy Sydarskiy <ya.donny@gmail.com>
2019-07-30 13:20:11 +0300

    sixth add all features

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
-// const text = document.getElementById('input').value.trim();
-
-// console.log(text);
-// let x = '';
-// const text = (function() {
-//     x = document.querySelector('input').addEventListener('keydown', function(e) {
-//    if (e.keyCode === 13) {
-//         return x;
-//    }
-//     });
-//   })();
-
-//   console.log(x);
-
+// By pressing the enter, the value of the entered data is determined in the input,
+//  new Li are created to which this value is transmitted ________________________
 
 const enter = input.addEventListener ("keypress", function (e) {
     if (e.keyCode === 13) {
         const text = document.getElementById('input').value.trim();
-        document.getElementById("input").value = "";
-        let d1 = document.getElementById('input'); 
-        d1.insertAdjacentHTML('afterend', '<div id="inser2" class="inser2"></div>');
-        d1.insertAdjacentHTML('afterend', '<div id="wrapper" class="wrapper"></div>');
-        let d2 = document.getElementById('wrapper');
-        d2.insertAdjacentHTML('afterBegin', '<spin class="spin"><i class="fa fa-times" onclick="del()"></i></spin>');
-
-        const out = document.getElementById('inser2').innerHTML = text;
-        document.getElementById('inser2').classList.add("new-todo", "new");
+        document.getElementById('input').value = '';
+        
+        let newLi = document.getElementById('tameplate');
+        let liClone = newLi.cloneNode(true);
+        newLi.parentNode.appendChild(liClone);
+        newLi.classList.add("block");
+        let textP = document.getElementById('inputText');
+        textP.innerHTML = text;  
     }
 });
 
+
+// By clicking, the current 'li' will be deleted.__________________________________
+
 const del = () => {
-    inser2.remove();
+    let newLi = document.getElementById('tameplate');
+    newLi.remove();
 };
 
 
